85e45d088325cef9e8c4ab5ea302590f51780031
Atam1283 <efedump@gmail.com>
2025-06-10 22:49:19 +0300

    Update data_sync.js

--- sync/data_sync.js
+++ sync/data_sync.js
@@ -1,1 +1,15 @@
+const firebase = require('firebase/app');
+require('firebase/database');
 
+const firebaseConfig = require('../config/firebase_config.json');
+firebase.initializeApp(firebaseConfig);
+
+function syncUserData(userId, data) {
+  firebase.database().ref(`users/${userId}`).set(data);
+}
+
+function getUserData(userId) {
+  return firebase.database().ref(`users/${userId}`).once('value');
+}
+
+module.exports = { syncUserData, getUserData };
